b3702f99fb8c25a8be6bee80c426bc79022f1535
Vincent <vincent.granie@2018.icam.fr>
2018-01-29 22:45:13 +0100

    nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -21,7 +21,15 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn.naive_bayes import GaussianNB
 
+clf = GaussianNb()
+
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+print clf.score(features_test, labels_test)
 
 
 #########################################################
