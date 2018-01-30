e0b17b5cc66c0f34365d11df554a2e054dd2f62f
Vincent <vincent.granie@2018.icam.fr>
2018-01-30 09:18:24 +0100

    nb_author_id

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -23,9 +23,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 from sklearn.naive_bayes import GaussianNB
 
-clf = GaussianNb()
+clf = GaussianNB()
+
+t0=time()
 clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1=time()
 pred = clf.predict(features_test)
+print "predict time;", round(time()-t1, 3), "s"
+
 print (clf.score(features_test, labels_test))
 
 from sklearn.metrics import accuracy_score
