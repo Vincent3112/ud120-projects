df1e3939d3ed527a9c771e15409080491654d8d7
Vincent <vincent.granie@2018.icam.fr>
2018-02-02 15:24:00 +0100

    nb_author_id

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,6 +19,26 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn.svm import SVC
+
+clf = SVC(kernel="rbf")
+
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100]
+
+t0=time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 1000), "s"
+
+t1=time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 1000), "s"
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+print(acc)
+
 
 
 
